3a7177a0a94e24777c5f12e4ed26748136b22c6c
ibex Stefan <itbs@ich-habe-fertig.com>
2024-01-26 13:28:08 +0100

    neue Icons Komponente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Welcome from "./react/Welcome";
 import ListGroup from "./react/components/ListGroup";
+import Icons from "./react/components/Icons";
+
 const stadt = ['London', 'Berlin','Paris'];
 const namen = ['Stefan', 'Christian','Paul'];
 const alter = ['10', '20','42'];
@@ -8,8 +10,7 @@ const alter = ['10', '20','42'];
 function App() {
     return (
         <>
-
-
+        <Icons />
 {/* 
             <Welcome nane="Chris" age={23} />
             <Welcome nane="Stefan" age={42} />
---
+++ src/react/components/Icons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Icons () {
+    return (<>
+
+<h1>Text</h1>
+
+    </>)
+}
+
+export default Icons;
